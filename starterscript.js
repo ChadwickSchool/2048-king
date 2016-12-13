cc82fb9eb4bc79f64527eff6001716f0eea1fe0f
Spencer Hurst <shurst2019@chadwickschool.org>
2016-12-13 08:40:57 -0800

    wrote move tiles down function but does not work

--- starterscript.js
+++ starterscript.js
@@ -22,18 +22,18 @@ function setUpBoard(){
 		}
 		grid.push(innergrid);
 	}
-	
+	//this adds arandom tile
 	addTile();
-	
-}
 
+}
+//the reason we need 3 is because the math function spawns a random # between 0,1 and multipleies by 3 and rounds
 function addTile() {
-	//place a 2 on a random spot in the board	
+	//place a 2 on a random spot in the board
 	var x = Math.round(Math.random()*3);
 	var y = Math.round(Math.random()*3);
 	grid[x][y] = "2";
 }
-	
+
 
 function printBoard(){
 	var board = '<br/>' + "*--------------*" + '<br/>';
@@ -46,32 +46,33 @@ function printBoard(){
 		board += "*--------------*";
 		board += '<br/>';
 	}
-	
+
 	//console.log(board);
 	document.getElementById("container").innerHTML = board;
 }
 
 
-//function gets called anytime  a key is pressed 
+//function gets called anytime  a key is pressed
 //e is a special variable
-// that references the event obeject that reads if the user is interacting with 
-//the window 
+// that references the event obeject that reads if the user is interacting with
+//the window
 document.onkeydown = function(e) {
-     
-    //makes it work in internet explorer which uses window.event and not e 
+
+    //makes it work in internet explorer which uses window.event and not e
     e = e || window.event;
 
     //keyCode is actually a character value which we convert to a String
-    //to use triple equals sign 
+    //to use triple equals sign
     if (e.keyCode == UP_ARROW) {
         // up arrow
         moveTilesUp();
-        
+
     }
-    //double equals sign will convert it for us 
+    //double equals sign will convert it for us
     else if (e.keyCode == DOWN_ARROW) {
         // down arrow
-        console.log("Pressed down");
+        moveTilesDown();
+				console.log("Pressed Down");
     }
     else if (e.keyCode == LEFT_ARROW) {
        // left arrow
@@ -80,14 +81,15 @@ document.onkeydown = function(e) {
     else if (e.keyCode == RIGHT_ARROW) {
        // right arrow
        console.log("Pressed right");
-    } 
-    
+    }
+
     printBoard(); //have to recall print board to get the board to update
 };
 
+//STORAGE OF MOVEMENT FUNCTIONS_________________________
 function moveTilesUp()
 {
-    
+
     for(var r=0; r < grid.length; r++)
     {
         for(var c=0; c<grid[r].length; c++)
@@ -97,9 +99,23 @@ function moveTilesUp()
                 grid[r-1][c] = grid[r][c];
                 grid[r][c] = "x";
             }
-            
+
         }
-        
-    }   
-    
+
+    }
+
+}
+function moveTilesDown()
+{
+	for(var r=3; r >=0; r-=1)
+	{
+		for(var c=0; c<grid[r].legnth; c++)
+		{
+			if(r !== 3 && grid[r][c] !== "x" && grid[r+1][c] === "x")
+			{
+					grid[r+1][c] = grid[r][c];
+					grid[r][c] = "x";
+				}
+			}
+		}
 }
